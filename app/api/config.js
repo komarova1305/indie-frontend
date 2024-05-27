bd13b3c8eebf80731febc2972a2f3a7c42e69b6b
Комарова Мария <149534401+komarova1305@users.noreply.github.com>
2024-05-27 15:55:37 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://komarova-backend.nomoredomainswork.ru/pindie-backend/api' 
+export const BASE_URL = 'https://komarova-backend.nomoredomainswork.ru/api' 
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
