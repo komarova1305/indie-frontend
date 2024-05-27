1185dd369970e3dccc754ccda2a90afad7cfe08c
Комарова Мария <149534401+komarova1305@users.noreply.github.com>
2024-05-27 19:25:41 +0300

    Delete app directory

--- app/api/config.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'https://komarova-backend.nomoredomainswork.ru/api' 
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/me`,
-};
